7e13c7266a989c3969e8f778de95c69e80f0c7d3
Gonzalo Dichiazza <gonzalodichiazza2@gmail.com>
2024-09-30 17:58:09 -0300

    Consumiendo api de la nasa con then y catch y async await

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
 
-const url = "https://api.nasa.gov/planetary/apod?api_key=Md6ZhB8e1s2GDDDn6dEI4WialPr963Fl4DVh6PcA&count=15" 
- 
-
-async function listaImagenes() {
-
+const url ="https://api.nasa.gov/planetary/apod?api_key=Md6ZhB8e1s2GDDDn6dEI4WialPr963Fl4DVh6PcA&count=15";
+ /*
+async function  imagenes(){
     try{
-        let fetchImagen = await fetch(url)
-        let datosImagenes = await fetchImagen.json()
+    const imagenUrl = await fetch(url); 
+    const imagenLista = await imagenUrl.json();
+    console.log(imagenLista); 
+    const posicion = document.querySelector("[data-ul]");
 
-        console.log(datosImagenes)
+    imagenLista.forEach(element => {
+        const contenido = 
+        `
+        <li class="card">
+        <img class="card__image" src=${element.url} alt="imagen">
+        <h3 class="card__title">${element.title}</h3>
+        </li>
+        `
 
-        const card = document.querySelector("[data-ul]")
+        posicion.innerHTML = posicion.innerHTML + contenido;
+    });
+    }   
+    catch(error){
+        console.log(error);
+    }
+    
 
-        datosImagenes.forEach( elemento => {
+    
+}
+*/
 
-            const contenido =
-            `<li class="card">
-                <img class="card__image" src="${elemento.url}" alt="imagen">
-                <h3 class="card__title">${elemento.title}</h3>
+function imagenes () {
+    const posicion = document.querySelector("[data-ul]");
+    fetch(url)
+    .then(res => res.json())
+    .then(datosImagenes => {
+        datosImagenes.forEach(element => {
+            const contenido = 
+            `
+            <li class="card">
+            <img class="card__image" src=${element.url} alt="imagen">
+            <h3 class="card__title">${element.title}</h3>
             </li>
             `
-
-            card.innerHTML = card.innerHTML + contenido
-        })
-        
-    }
-    catch(error){
-        console.log(error)
-    }
-}
-
-listaImagenes()
-
-
-// then y catch
-/* function listaImagenes(){
-    fetch(url)
-    .then( response => response.json())
-    .then( datosImagenes => {
-        console.log(datosImagenes)
-
-        const card = document.querySelector("[data-ul]")
-
-        datosImagenes.forEach( elemento => {
-            const contenido =`<li class="card">
-            <img class="card__image" src="${elemento.url}" alt="imagen">
-            <h3 class="card__title">${elemento.title}</h3>
-        </li>
-            `
-            card.innerHTML = card.innerHTML + contenido
-        })
+            posicion.innerHTML = posicion.innerHTML + contenido;
+        });
     })
-    .catch( error => console.log(error))
+    .catch(error)
+       
 }
 
-listaImagenes() */
+imagenes();
